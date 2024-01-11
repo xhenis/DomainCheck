0f7c8a335f6229e41fdde0ede8e38309a56115b9
Xhenis Osmani <xhenis.osmani@outlook.com>
2024-01-11 15:01:35 +0100

    DKIM, SPF, DMAC checker

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import argparse
+import dns.resolver
+
+parser = argparse.ArgumentParser(description='Simple DMARC DKIM SPF quick test.')
+parser.add_argument('domain', help='Domain name to test')
+parser.add_argument('selector', help='DKIM Selector, can be extracted from email')
+args = parser.parse_args()
+domain = args.domain
+selector = args.selector
+
+print()
+print ("Testing domain", domain, "for DKIM record with selector", selector, "...")
+try:
+  test_dkim = dns.resolver.resolve(selector + '._domainkey.' + domain , 'TXT')
+  for dns_data in test_dkim:
+    if 'DKIM1' in str(dns_data):
+      print ("  [PASS] DKIM record found  :",dns_data)
+except:
+  print ("  [FAIL] DKIM record not found.")
+  pass
+
+print()
+print ("Testing domain", domain, "for SPF record...")
+try:
+  test_spf = dns.resolver.resolve(domain , 'TXT')
+  for dns_data in test_spf:
+    if 'spf1' in str(dns_data):
+      print ("  [PASS] SPF record found   :",dns_data)
+except:
+  print ("  [FAIL] SPF record not found.")
+  pass
+
+print()
+print ("Testing domain", domain, "for DMARC record...")
+try:
+  test_dmarc = dns.resolver.resolve('_dmarc.' + domain , 'TXT')
+  for dns_data in test_dmarc:
+    if 'DMARC1' in str(dns_data):
+      print ("  [PASS] DMARC record found :",dns_data)
+except:
+  print ("  [FAIL] DMARC record not found.")
+  pass
