b5e86c8fbd7edb8fd09856a80cf1338cd5c55acb
Xhenis Osmani <xhenis.osmani@outlook.com>
2024-01-12 13:38:21 +0100

    Multiple domain check and export to XLSX - styled

--- main.py
+++ main.py
@@ -1,45 +1,75 @@
 #!/usr/bin/python3
 import argparse
 import dns.resolver
+import pandas as pd
 
-parser = argparse.ArgumentParser(description='Simple DMARC DKIM SPF quick test.')
-parser.add_argument('domain', help='Domain name to test')
-parser.add_argument('selector', help='DKIM Selector, can be extracted from email')
-args = parser.parse_args()
-domain = args.domain
-selector = args.selector
+# Open the file in read mode
+with open('domains.txt', 'r') as file:
 
-print()
-print ("Testing domain", domain, "for DKIM record with selector", selector, "...")
-try:
-  test_dkim = dns.resolver.resolve(selector + '._domainkey.' + domain , 'TXT')
-  for dns_data in test_dkim:
-    if 'DKIM1' in str(dns_data):
-      print ("  [PASS] DKIM record found  :",dns_data)
-except:
-  print ("  [FAIL] DKIM record not found.")
-  pass
+    content = file.read()
 
-print()
-print ("Testing domain", domain, "for SPF record...")
-try:
-  test_spf = dns.resolver.resolve(domain , 'TXT')
-  for dns_data in test_spf:
-    if 'spf1' in str(dns_data):
-      print ("  [PASS] SPF record found   :",dns_data)
-except:
-  print ("  [FAIL] SPF record not found.")
-  pass
+    domain_list = content.split(', ')
+    spf_list = []
+    dmarc_list = []
 
-print()
-print ("Testing domain", domain, "for DMARC record...")
-try:
-  test_dmarc = dns.resolver.resolve('_dmarc.' + domain , 'TXT')
-  for dns_data in test_dmarc:
-    if 'DMARC1' in str(dns_data):
-      print ("  [PASS] DMARC record found :",dns_data)
-except:
-  print ("  [FAIL] DMARC record not found.")
-  pass
+for domain in domain_list:
+    parser = argparse.ArgumentParser(description='Simple DMARC DKIM SPF quick test.')
+    # print()
+    # print ("Testing domain", domain, "for DKIM record with selector", selector, "...")
+    # try:
+    #   test_dkim = dns.resolver.resolve(selector + '._domainkey.' + domain , 'TXT')
+    #   for dns_data in test_dkim:
+    #     if 'DKIM1' in str(dns_data):
+    #       print ("  [PASS] DKIM record found  :",dns_data)
+    # except:
+    #   print ("  [FAIL] DKIM record not found.")
+    #   pass
 
-print ("Done")
+    print()
+    print ("Testing domain", domain, "for SPF record...")
+    try:
+      test_spf = dns.resolver.resolve(domain , 'TXT')
+      for dns_data in test_spf:
+        if 'spf1' in str(dns_data):
+          # print ("  [PASS] SPF record found   :",dns_data)
+          result_spf = f"  [PASS] SPF record found: {dns_data}"
+    except:
+      # print ("  [FAIL] SPF record not found.")
+      result_spf = ("  [FAIL] SPF record not found.")
+      pass
+    
+    spf_list.append(result_spf)
+    print()
+    print ("Testing domain", domain, "for DMARC record...")
+    try:
+      test_dmarc = dns.resolver.resolve('_dmarc.' + domain , 'TXT')
+      for dns_data in test_dmarc:
+        if 'DMARC1' in str(dns_data):
+          # print ("  [PASS] DMARC record found :",dns_data)
+          result_dmarc = f"  [PASS] DMARC record found: {dns_data}"
+    except:
+      # print ("  [FAIL] DMARC record not found.")
+      result_dmarc = ("  [FAIL] DMARC record not found.")
+      pass
+    
+    dmarc_list.append(result_dmarc)
+    print ("Done")
+    
+data = {"Doamin name": domain_list,
+        "SPF record": spf_list,
+        "DMARC record": dmarc_list}
+
+df = pd.DataFrame(data)
+
+def highlight_cells(val):
+    if 'PASS' in val:
+        return 'color: green'
+    elif 'FAIL' in val:
+        return 'color: red'
+    else:
+        return ''
+
+styled_df = df.style.applymap(highlight_cells, subset=['SPF record', 'DMARC record'])
+
+# Save the styled DataFrame to an Excel file
+styled_df.to_excel(r'export_dataframe_styled.xlsx', index=False)
